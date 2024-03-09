9daf1b73c9ad5df7e926514859b80f0c7dbd3196
GUOKKK <528032175@qq.com>
2024-03-09 20:24:22 +0800

    生成config.js文件模版名。及其相关文件暴露。

--- packages/ggbee-fe-lint/src/lints/eslint/index.ts
+++ packages/ggbee-fe-lint/src/lints/eslint/index.ts
@@ -1,3 +1,4 @@
 export * from './getESLintConfig';
+export * from './getESLintConfigType';
 export * from './formatESLintResults';
 export * from './doESLint';
--- packages/ggbee-fe-lint/src/lints/stylelint/index.ts
+++ packages/ggbee-fe-lint/src/lints/stylelint/index.ts
@@ -1,3 +1,4 @@
 export * from './getStylelintConfig';
+export * from './getStylelintDocUrl';
 export * from './formatStylelintResults';
 export * from './doStylelint';
