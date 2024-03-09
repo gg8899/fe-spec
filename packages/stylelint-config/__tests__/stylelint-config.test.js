cc848034f4d62ef397e9a729b3711c5ac43a7d15
GUOKKK <528032175@qq.com>
2024-03-09 15:38:57 +0800

    stylelint test

--- packages/stylelint-config/__tests__/stylelint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const stylelintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(stylelintConfig(), 'Hello from stylelintConfig');
-console.info('stylelintConfig tests passed');
--- packages/stylelint-config/lib/stylelint-config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = stylelintConfig;
-
-function stylelintConfig() {
-  return 'Hello from stylelintConfig';
-}
