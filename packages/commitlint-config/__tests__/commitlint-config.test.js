ef62a3f7c74dfaa427ef77de9855454b7852c4e0
GUOKKK <528032175@qq.com>
2024-03-09 15:27:39 +0800

    markdownlint配置

--- packages/commitlint-config/__tests__/commitlint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const commitlintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(commitlintConfig(), 'Hello from commitlintConfig');
-console.info('commitlintConfig tests passed');
--- packages/markdownlint-config/__tests__/markdownlint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const markdownlintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(markdownlintConfig(), 'Hello from markdownlintConfig');
-console.info('markdownlintConfig tests passed');
